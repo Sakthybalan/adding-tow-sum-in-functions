458a18484dbcf34621ba074ac363dd91a4dab285
Sakthybalan <sakthybalan.mc21@bitsathy.ac.in>
2024-09-11 18:31:04 +0530

    class function

---
+++ furnichers.java
@@ -0,0 +1,24 @@
+public class furnichers {
+    int chair=0;
+    int table =0;
+    int fan=0;
+    int sofa=0;
+    int cart=0;
+    public static void main(String[] args) {
+        furnichers shop1=new furnichers();
+        shop1.chair=1000;
+        shop1.table=16000;
+        shop1.fan=1600;
+        shop1.sofa=36456;
+        shop1.cart=2000;
+        System.out.println(shop1.sofa);
+        furnichers shop2=new furnichers();
+        shop2.chair=1000;
+        shop2.table=16000;
+        shop2.fan=1600;
+        shop2.sofa=36000;
+        shop2.cart=2000;
+        System.out.println(shop2.sofa);
+
+    }
+}
