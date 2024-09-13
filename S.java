3cd04ec59e8aed72172c5781d3417380f03d0e10
Sakthybalan <sakthybalan.mc21@bitsathy.ac.in>
2024-09-13 21:46:20 +0530

    adding two sum

---
+++ S.java
@@ -0,0 +1,9 @@
+public class S {
+    void sum(int a , int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        S um = new S();
+        um.sum(10, 10);
+    }
+}
